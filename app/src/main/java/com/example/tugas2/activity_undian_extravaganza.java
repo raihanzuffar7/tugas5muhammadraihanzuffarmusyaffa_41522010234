15ea84bee667a12c2709e224900ec76d8b1fda46
raihanzuffar7 <raihanzuffarmusyaffa@gmail.com git config --global user.name raihanzuffar7git config --global user.email raihanzuffarmusyaffa@gmail.com>
2024-05-29 13:30:05 +0700

    tugas5

---
+++ app/src/main/java/com/example/tugas2/activity_undian_extravaganza.java
@@ -0,0 +1,30 @@
+package com.example.tugas2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class scan_claimtutupbotol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_scan_claimtutupbotol);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void pindahspin(View view) {
+        Intent pindahspin = new Intent(scan_claimtutupbotol.this, extravaganza1.class);
+        startActivity(pindahspin);
+    }
+}
